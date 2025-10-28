9e980eb031982ae1034a63a68a573a3f92a90178
mildrette <mildredfonka1@maild.com>
2025-10-28 20:54:05 +0100

    lesson 1 day 1

---
+++ day1_hello_app.py
@@ -0,0 +1,15 @@
+# Mini Project — “Hello User App”
+
+print("=== Hello User App ===")
+
+name = input("what's your name?: ")
+age = int(input("How old are you?: "))
+
+print("my name is ", name)
+
+#  if esle statement
+
+if age >= 18 : 
+    print("I am an adult")
+else:
+    print("i am still a kid")
---
+++ day1_variables.py
@@ -0,0 +1,22 @@
+# Learning the Core Concept — Variables & Data Types
+
+name = "Zoe"
+age = 20
+isLearningPython = True
+
+print("Hello there my name is", name, "i am ", age , "years old.")
+print("Is she Learning python now??: ", isLearningPython)
+
+
+
+# Input and Simple Logic
+
+user_name = input("what's your name?? ")
+
+user_age = input("How old are you?? ")
+
+print("Hi", user_name, "you are ", user_age, " year old")
+
+
+
+
